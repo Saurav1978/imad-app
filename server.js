9484536ec0059aed3498f34e4f3b911f006184a6
Saurav1978 <30767927+Saurav1978@users.noreply.github.com>
2017-08-08 23:47:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ return htmlTemplate;
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {}content object for article one
-    var articleName = req. parans.articleName;
+    var articleName = req. params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
